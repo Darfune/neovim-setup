8c08c3b5c794893e7979652a0c9ad72a205f735a
Darfune <harolep@gmail.com>
2023-11-04 15:19:47 +0200

    keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,12 +1,25 @@
 
 local opts = { noremap = true, silent = true }
 
+local api_map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
+
 vim.g.mapleader = " "
 
 
 -- Expore main directory
-vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
+map("n","<leader>pv",vim.cmd.Ex)
 
-local map = vim.api.nvim_set_keymap
+api_map("n", '<M-1>', ':NvimTreeToggle<CR>', opts)
 
-map("n", '<M-1>', ':NvimTreeToggle<CR>', opts)
+map('v', "J", ":m '>+1<CR>gv=gv") -- Move highlighted lines down
+map('v', "K", ":m '<-2<CR>gv=gv") -- Move highlighted lines up
+
+map('n', 'J', 'mzJ`z') -- Don't change cursor position 
+
+map('n', '<C-d>', '<C-d>zz') -- Page down with the cursor at the center
+map('n', '<C-u>', '<C-u>zz') -- Page up with the cursor at the center
+
+
+map('x', '<leader>p', "\"_dP")
+
