ae9a288c11562fa960d5883f81899c40180fa5a6
Darfune <harolep@gmail.com>
2023-11-04 17:51:30 +0200

    go init setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,6 @@
+local util = require "lspconfig/util"
+
+
 local on_attach = function(client, bufnr)
 	local function buf_set_keymap(...)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
@@ -15,7 +18,25 @@ end
 ---@diagnostic disable-next-line: undefined-global
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
 require('lspconfig')['lua_ls'].setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
+})
+
+require('lspconfig')['gopls'].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = {"gopls"},
+	filetypes = {"go","gomod","gowork","gotmpl"},
+	root_dir = util.root_pattern("go.work","go.mod","git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	},
 })
