54d3de0da8d690a3cce05f5f3131ac0c06132a92
Darfune <harolep@gmail.com>
2023-11-14 17:45:52 +0200

    commenter added

--- lua/plugins/packer/init.lua
+++ lua/plugins/packer/init.lua
@@ -49,21 +49,26 @@ return require('packer').startup(function(use)
 	use {'romgrk/barbar.nvim'}
 	use {'onsails/lspkind-nvim'}
 
-			use {
-				"folke/which-key.nvim",
-				config = function()
-					vim.o.timeout = true
-					vim.o.timeoutlen = 300
-					require("which-key").setup {
-						-- your configuration comes here
-						-- or leave it empty to use the default settings
-						-- refer to the configuration section below
-					}
-				end
+	use {
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
 			}
+		end
+	}
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup()
+		end
+	}
 
-
-		end)
+end)
 
 
 
