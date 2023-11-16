0a1f4daa2db4e094f3ed0d522872bfc8dabe911e
Darfune <harolep@gmail.com>
2023-11-17 00:44:42 +0200

    toggle term keybings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -9,3 +9,13 @@ require'toggleterm'.setup {
 	direction = 'horizontal',
 	close_on_exit = true
 }
+
+  local opts = { noremap = true, silent = true }
+  function _G.set_terminal_keymaps()
+    vim.api.nvim_buf_set_keymap(0, "t", "<m-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<m-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<m-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<m-l>", [[<C-\><C-n><C-W>l]], opts)
+  end
+
+
