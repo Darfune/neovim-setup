9c3f5bf4af56497756cb7092367b6c4e4f39cc7d
Darfune <harolep@gmail.com>
2023-12-20 19:46:39 +0200

    color changes

--- lua/plugins/indent-blackline.lua
+++ lua/plugins/indent-blackline.lua
@@ -1,20 +1,20 @@
 local config = function()
 	local highlight = {
-		"RainbowRed",
-		"RainbowYellow",
+		"RainbowCyan",
+		"RainbowViolet",
+		"RainbowGreen",
 		"RainbowBlue",
 		"RainbowOrange",
-		"RainbowGreen",
-		"RainbowViolet",
-		"RainbowCyan",
+		"RainbowYellow",
+		"RainbowRed",
 	}
 
 	local hooks = require("ibl.hooks")
 	-- create the highlight groups in the highlight setup hook, so they are reset
 	-- every time the colorscheme changes
 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#F1FA8C" })
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#944046" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#8d9261" })
 		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
 		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
 		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
@@ -24,8 +24,6 @@ local config = function()
 	vim.g.rainbow_delimiters = { highlight = highlight }
 	require("ibl").setup({ indent = { highlight = highlight } })
 	hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
-
 end
 
 return {
